55b8af44bc6871e9651255977efadc5346bb3c67
Antoine-PETIT-Loco <antoine.petit@etu.univ-nantes.fr>
2021-03-19 17:02:45 +0100

    débur classe filtres

--- src/components/filtres.js
+++ src/components/filtres.js
@@ -5,7 +5,6 @@ Vue.component('filtre', {
     data: function () {
         return {
             request: '',
-            search: '',
 
             African: '',
             American: '',
@@ -39,17 +38,44 @@ Vue.component('filtre', {
 
         handleSubmit: function () {
             this.createRequest();
+            this.deletefiler();
             useRecipeApi.byFiltre(this.request).then(data => this.$emit('search-done', data.results));
+
+        },
+        deletefiler: function (){
+            this.African = '';
+            this.American= '';
+            this.British= '';
+            this.Cajun= '';
+            this.Caribbean = '';
+            this.Chinese= '';
+            this.Eastern_European= '';
+            this.France= '';
+            this.German= '';
+
+            this.dairy= '';
+            this.peanut= '';
+            this.soy= '';
+            this.egg= '';
+            this.seafood= '';
+            this.sulfite= '';
+            this.gluten= '';
+            this.sesame= '';
+            this.treenut= '';
+
+            this.ingredients= '';
+            this.ingredientsExclu= '';
+            this.minCalories= '';
+            this.maxCalories= '';
+            this.diet= '';
+            this.diettype= '';
         },
 
         createRequest: function (){
-            let request = 'https://api.spoonacular.com/recipes/complexSearch?`';
+            let request = 'https://api.spoonacular.com/recipes/complexSearch?';
             let pays = '';
             let intolerance = '';
 
-            if (this.search !== ''){
-                request += '&titleMatch=' + this.search
-            }
             if (this.ingredients !== ''){
                 request += '&includeIngredients=' + this.ingredients
             }
@@ -73,65 +99,65 @@ Vue.component('filtre', {
             }
 
             if (this.African !== ''){
-                pays += this.African +',';
+                pays += 'African,';
             }
             if (this.American !== ''){
-                pays += this.American +',';
+                pays += 'American,';
             }
             if (this.British !== ''){
-                pays += this.British +',';
+                pays += 'British,';
             }
             if (this.Cajun !== ''){
-                pays += this.Cajun +',';
+                pays += 'Cajun,';
             }
             if (this.Caribbean !== ''){
-                pays += this.Caribbean +',';
+                pays += 'Caribbean,';
             }
             if (this.Chinese !== ''){
-                pays += this.Chinese +',';
+                pays += 'Chinese,';
             }
             if (this.Eastern_European !== ''){
-                pays += this.Eastern_European +',';
+                pays += 'Eastern_European,';
             }
             if (this.France !== ''){
-                pays += this.France +',';
+                pays += 'French,';
             }
             if (this.German !== ''){
-                pays += this.German +',';
+                pays += 'German,';
             }
             if (pays !== ''){
-                request += '&country=' + pays;
+                request += '&cuisine=' + pays;
             }
 
             if (this.dairy !== ''){
-                intolerance += this.dairy +',';
+                intolerance += 'dairy,';
             }
             if (this.peanut !== ''){
-                intolerance += this.peanut +',';
+                intolerance += 'peanut,';
             }
             if (this.soy !== ''){
-                intolerance += this.soy +',';
+                intolerance += 'soy,';
             }
             if (this.egg !== ''){
-                intolerance += this.egg +',';
+                intolerance += 'egg,';
             }
             if (this.seafood !== ''){
-                intolerance += this.seafood +',';
+                intolerance += 'seafood,';
             }
             if (this.sulfite !== ''){
-                intolerance += this.sulfite +',';
+                intolerance += 'sulfite,';
             }
             if (this.gluten !== ''){
-                intolerance += this.gluten +',';
+                intolerance += 'gluten,';
             }
             if (this.sesame !== ''){
-                intolerance += this.sesame +',';
+                intolerance += 'sesame,';
             }
             if (this.treenut !== ''){
-                intolerance += this.treenut +',';
+                intolerance += 'treenut,';
             }
             if (intolerance !== ''){
-                request += '&intolerances =' + intolerance;
+                request += '&intolerances=' + intolerance;
             }
 
             request += '&apiKey=b51ad72a29914fb4b0ce97a4be312061&number=2'
