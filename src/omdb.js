255ce620e583d4623ef1906ca0242a5245724e54
Antoine-PETIT-Loco <antoine.petit@etu.univ-nantes.fr>
2021-03-19 11:40:56 +0100

    filtre

--- src/omdb.js
+++ src/omdb.js
@@ -18,8 +18,17 @@ const useRecipeApi = {
                 .then(jsonResponse => resolve(jsonResponse))
             .catch((err) => reject(err))
     }),
-    byFiltre : (search) =>new Promise((resolve , reject) => {
-        const RECIPE_API_URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=2&query=${search}`;
+    byFiltre : (search, filtre, val) =>new Promise((resolve , reject) => {
+        if (filtre === "maxCalories" && val !== 0)
+            const RECIPE_API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=${search}&maxCalories=${val}&apiKey=${API_KEY}&number=2`;
+        else if (filtre === "vegan")
+            const RECIPE_API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=${search}&vegan=true&apiKey=${API_KEY}&number=2`;
+        else if (filtre === "vegetarian")
+            const RECIPE_API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=${search}&vegetarian=true&apiKey=${API_KEY}&number=2`;
+        else if (filtre === "glutenFree")
+            const RECIPE_API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=${search}&glutenFree=true&apiKey=${API_KEY}&number=2`;
+        else
+            const RECIPE_API_URL = `https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&number=2&query=${search}`;
 
         fetch(RECIPE_API_URL)
             .then(response => response.json())
